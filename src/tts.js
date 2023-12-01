bdb6820ccd1791548aa40653c8ae5263908500a7
Furkan Tezkurul <ftezkuru@mail.uni-mannheim.de>
2023-12-01 14:57:49 +0100

    API key

---
+++ src/tts.js
@@ -0,0 +1,29 @@
+
+require('dotenv').config();
+
+const ElevenLabs = require("elevenlabs-node");
+
+let textInput = "I am a test";
+
+const voice = new ElevenLabs(
+    {
+        apiKey:  process.env.ELEVENLABS_API_KEY, // Your API key from Elevenlabs
+        voiceId: "pNInz6obpgDQGcFmaJgB",             // A Voice ID from Elevenlabs
+    }
+);
+
+voice.textToSpeech({
+    // Required Parameters
+    fileName:        "audio.mp3",                    // The name of your audio file
+    textInput:       textInput,                // The text you wish to convert to speech
+
+    // Optional Parameters
+    voiceId:         "21m00Tcm4TlvDq8ikWAM",         // A different Voice ID from the default
+    stability:       0.5,                            // The stability for the converted speech
+    similarityBoost: 0.5,                            // The similarity boost for the converted speech
+    modelId:         "elevenlabs_multilingual_v2",   // The ElevenLabs Model ID
+    style:           1,                              // The style exaggeration for the converted speech
+    speakerBoost:    true                            // The speaker boost for the converted speech
+  }).then((res) => {
+    console.log(res);
+});
