722d272e5d47e2a3dd64f207102674b387f9e185
Furkan Tezkurul <ftezkuru@mail.uni-mannheim.de>
2023-12-03 15:42:09 +0100

    formatting

--- src/tts.js
+++ src/tts.js
@@ -4,20 +4,20 @@ const ElevenLabs = require("elevenlabs-node");
 const voice = new ElevenLabs(
     {
         apiKey:  process.env.ELEVENLABS_API_KEY, // Your API key from Elevenlabs
-        voiceId: "pNInz6obpgDQGcFmaJgB",             // A Voice ID from Elevenlabs
+        voiceId: "pNInz6obpgDQGcFmaJgB",         // A Voice ID from Elevenlabs
     }
 );
 
 voice.textToSpeech({
     // Required Parameters
-    fileName:        "audios/audio2.mp3",                    // The name of your audio file
+    fileName:        "audios/audio2.mp3",            // The name of your audio file
     textInput:       "mozzy is cool",                // The text you wish to convert to speech
 
     // Optional Parameters
     voiceId:         "pNInz6obpgDQGcFmaJgB",         // A different Voice ID from the default
     stability:       0.5,                            // The stability for the converted speech
     similarityBoost: 0.5,                            // The similarity boost for the converted speech
-    modelId:         "eleven_monolingual_v1",       // The ElevenLabs Model ID
+    modelId:         "eleven_monolingual_v1",        // The ElevenLabs Model ID
     style:           1,                              // The style exaggeration for the converted speech
     speakerBoost:    true                            // The speaker boost for the converted speech
   }).then((res) => {
